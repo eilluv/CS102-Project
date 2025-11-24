8f4b8ac03e3f6f72ec549679c00735dd2f9ca22d
faisalmj777 <aljadaanfaisal21@gmail.com>
2025-11-24 18:14:58 +0300

    Update Cs102project.java
    Added initialization and registration for two workshop events.

--- src/main/java/cs102_project/Cs102project.java
+++ src/main/java/cs102_project/Cs102project.java
@@ -18,5 +18,17 @@ public class Cs102project {
     public static void main(String[] args) {
         // Your main method code here
         // (Keep all the main application logic but remove the class/interface definitions)
+          System.out.println("Event Management System Initialized ");
+
+        WorkshopEvent ws1 = new WorkshopEvent("WS001", "CyberSecurity Event", 15, "Cloud & Data Security ", "Dr. Mohammed");
+        ws1.registerAttendee("Ibrahim");
+        ws1.registerAttendee("Abdullah");
+        ws1.registerAttendee("Bader");
+        eventRegistry.put(ws1.getId(), ws1);
+
+        WorkshopEvent ws2 = new WorkshopEvent("WS002", "Developers Event", 10, "Web Development", "Mr. Ahmed");
+        ws2.registerAttendee("Faisal");
+        ws2.registerAttendee("Rakan");
+        eventRegistry.put(ws2.getId(), ws2);
     }
 }
